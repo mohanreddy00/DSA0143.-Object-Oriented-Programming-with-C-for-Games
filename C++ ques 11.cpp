90801ca78005458876bd4ccaf52895d3198c4565
mohanreddy00 <113960098+mohanreddy00@users.noreply.github.com>
2023-02-17 09:46:58 +0530

    Create C++ ques 11.cpp

---
+++ C++ ques 11.cpp
@@ -0,0 +1,41 @@
+// C++ program to find cubic root of a number
+
+// using Binary Search
+
+#include <bits/stdc++.h>
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+
+    int n = 28, sum = 0;
+
+    
+
+    for(int i = 1; i < n; i++){
+
+        if(n % i == 0)
+
+            sum = sum + i;
+
+    }
+
+    
+
+    if(sum == n)
+
+        cout << n << " is a perfect number";
+
+    else
+
+        cout << n << " is not a perfect number";
+
+    
+
+}
